6ecb2d38a874dad9e646a47c4b876dc28858bcf8
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-11-05 19:25:17 +0530

    Add files via upload

---
+++ Bit Manipulation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int x = 5, y = 4;
+	printf("%d ", x&y);
+	printf("%d ", x|y);
+	printf("%d ", ~x);
+	printf("%d ", ~y);
+	printf("%d ", x^y);
+
+
+}
