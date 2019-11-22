cc2596a3ab66c7d20d464823eaa82ed4b2d02193
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-11-23 00:49:56 +0530

    find max element and second max element

---
+++ 5 Arrays/02 max and second max.py
@@ -0,0 +1,14 @@
+def largestAndSecondLargest(sizeOfArray, arr):
+    max1 = -1
+    max2 = -1
+    
+    for i in arr:
+        if i > max1:
+            max2 = max1
+            max1 = i
+        elif i > max2:
+            if i != max1:
+                max2 = i
+            
+    
+    print (max1, max2)
