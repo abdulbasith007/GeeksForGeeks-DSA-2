6ae002010e54af74740b3cb7daeda9ce9f5b62cb
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-11-24 20:15:55 +0530

    Update 06 rotate array.py

--- 5 Arrays/06 rotate array.py
+++ 5 Arrays/06 rotate array.py
@@ -11,3 +11,28 @@ for _ in range(tot):
         print(x, end = ' ')
     print()
     
+
+    
+    
+#######################################
+##  Array rotation using reverse():  ##
+#E#####################################
+
+    
+def reverse(arr, low, high):
+    
+    while low<high:
+        arr[low], arr[high] = arr[high], arr[low]
+        low += 1
+        high -= 1
+    
+    return arr
+
+def rotateArr(arr,d,n):
+
+  reverse(arr, 0, d-1)
+  reverse(arr,d,n-1)
+  reverse(arr,0,n-1)
+
+  return arr
+    
