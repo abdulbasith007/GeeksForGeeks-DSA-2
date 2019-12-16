b34e3a8b09479717f0f3d8b765b9c3ab3708b70f
Fazeel Usmani <fazeelusmani18@gmail.com>
2019-12-16 23:41:34 +0530

    working for all test cases

---
+++ 5 Arrays/17 stock buy and sell.cpp
@@ -0,0 +1,84 @@
+// As the Python language doesn't support zero spacing and we cannot print the expected output in the expected format, I'm writing the same solution in CPP.
+// Same as Python SOlution
+
+
+#include <stdio.h>
+
+// solution structure
+struct Interval
+{
+    int buy;
+    int sell;
+};
+ 
+// This function finds the buy sell schedule for maximum profit
+void stockBuySell(int price[], int n)
+{
+    // Prices must be given for at least two days
+    if (n == 1)    
+        return;
+ 
+    int count = 0; // count of solution pairs
+ 
+    // solution vector
+    Interval sol[n/2 + 1];
+ 
+    // Traverse through given price array
+    int i = 0;
+    while (i < n-1)
+    {
+        // Find Local Minima. Note that the limit is (n-2) as we are
+        // comparing present element to the next element. 
+        while ((i < n-1) && (price[i+1] <= price[i]))
+            i++;
+ 
+        // If we reached the end, break as no further solution possible
+        if (i == n-1)
+            break;
+ 
+        // Store the index of minima
+        sol[count].buy = i++;
+ 
+        // Find Local Maxima.  Note that the limit is (n-1) as we are
+        // comparing to previous element
+        while ((i < n) && (price[i] >= price[i-1]))
+            i++;
+ 
+        // Store the index of maxima
+        sol[count].sell = i-1;
+ 
+        // Increment count of buy/sell pairs
+        count++;
+    }
+ 
+    // print solution
+    if (count == 0)
+        printf("No Profit");
+    else
+    {
+       for (int i = 0; i < count; i++)
+          printf("(%d %d) ", sol[i].buy, sol[i].sell);
+    }
+    printf("\n");
+    return;
+}
+ 
+// Driver program to test above functions
+int main()
+{   
+    
+    int price[10000],n,i,T;
+    
+    scanf("%d",&T);
+    
+    while(T--){
+        
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+      scanf("%d",&price[i]);
+    // fucntion call
+    stockBuySell(price, n);
+    }
+    return 0;
+}
